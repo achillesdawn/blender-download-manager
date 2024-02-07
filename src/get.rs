93780baabbb75176a85e841c426f7ba00e24ebdc
miguel <vackap@gmail.com>
2024-02-07 01:01:09 -0400

    colorized

--- src/get.rs
+++ src/get.rs
@@ -1,4 +1,3 @@
-use std::error::Error;
 use std::fs::{remove_file, File};
 use std::io::Write;
 use std::time::Instant;
@@ -9,6 +8,10 @@ use ureq::Request;
 use crate::io::save_results;
 use crate::select::{self};
 use crate::select::{Album, Media};
+
+use anyhow::Result;
+
+use colored::Colorize;
 
 const COOKIE: &str = "disclaimer=eyJpdiI6ImNTUVhjQmhIR0hXUGlxVStLS0xaSlE9PSIsInZhbHVlIjoiY0pvRlZ6QmMxSDFMbmdKam1wVWVmUT09IiwibWFjIjoiMzFiNzMxOTYzOTllNjMzN2EwYTE0OWE1ZDNiNGViZjVhZTc5ZTIzM2VmMTkwNTU0MDViODYxNGE4OWNiMmMzYiJ9; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IjI4UVRaVmhiZ09KUlNkUm9KZURTOHc9PSIsInZhbHVlIjoiWHhZeE1lTCt2TW5IRHQrdVVqUkIzM3lrSURyaFhNaDJyVmtYdnQrMkR2aDRMdVhMYUI5ajI5RWZNSjdTUm5pWXZEUlc1TDFRNjhBSTNhelp2RzZrVDdnWDhCbFNERzNva1VYZENHNjkybVU9IiwibWFjIjoiZjY1MTQ4N2M0MDg2YWE2ZTk2MjkwN2Y5MGE2ZDFjMTA0NjdmYmZhZjc5MTNmNmMzZDdmOGQ2ZmJkM2RjODQ4OCJ9; XSRF-TOKEN=eyJpdiI6IlwvKzNcL1lUSGlxZ2FVOW8ycWZxMzJrdz09IiwidmFsdWUiOiJvOE8rN2txZlFuVk5uTHU0dnhoeWU5clhRSUUzXC9UNzl1YW1wdXRnSnF4Ym1GcjdyTTNHXC9tOURQMjlYM2VrWlh0d3dqUGlHSDdvd3BYXC9IaTNPWDhHQT09IiwibWFjIjoiM2U3YTU5ZTFkZmI0MGY2ODk1NTI5MDdlMmYzYTllZDMxZTM1NTc1OTY1YmM1MjRiMDVhMjYyN2Y4NTFmZTZmNSJ9; laravel_session=eyJpdiI6IjZ3azVVeDJPanVEa2FOK2NYb0hIbnc9PSIsInZhbHVlIjoiTHZvU1hmNSs0OWdqdmx3RkJGTkRmbWs2QWFPWG1JQTluQ0V6ZStQRWtRQlROUnkyZ2VHS09vQ1NpUnlPeTNKdzhOaW8zUVFWdjdhTE41Z1ZaYzdBR0E9PSIsIm1hYyI6IjU1NDBjZjFhMzQ4NjIyZTdhODk3NmYzM2U4NTI3MmUxM2ZjMmMzYWI5MWJiZTAyYjZiM2FkNGFhMjU2YWIzNDIifQ%3D%3D; _ga_6S5PBWQ8CG=GS1.1.1707261381.1.0.1707261381.0.0.0; _ga=GA1.1.686881403.1707261382";
 
@@ -78,7 +81,7 @@ fn get_saved() {
     save_results(results).unwrap();
 }
 
-pub fn get_album(album: &Album) -> Result<String, Box<dyn Error>> {
+pub fn get_album(album: &Album) -> Result<String> {
     let getter = Getter::new(&album.link);
 
     let r = match getter.request.call() {
@@ -95,9 +98,7 @@ pub fn get_album(album: &Album) -> Result<String, Box<dyn Error>> {
     Ok(body)
 }
 
-pub fn download(link: &str, file: &mut File) -> Result<(), String> {
-
-
+pub fn download(link: &str, file: &mut File) -> Result<()> {
     let getter = Getter::new(link);
 
     let r = getter.request.call().expect("Could not GET url");
@@ -105,12 +106,12 @@ pub fn download(link: &str, file: &mut File) -> Result<(), String> {
     assert!(r.has("Content-Length"));
 
     let len: usize = r.header("Content-Length").unwrap().parse().unwrap();
-    let len_mb = len as f32/1000000.0;
-    println!("Content Size {len_mb:.1}mb");
+    let len_mb = len as f32 / 1000000.0;
+    println!("{} {len_mb:.1}mb ({len} bytes)", "Content Size".blue());
 
     if len_mb > 50.0 {
-        println!("Content too big {:.1}mb, skipping...", len);
-        return Err("TOO_BIG".into());
+        println!("{} {:.1}mb, skipping...", "Content >50mb big".red(), len_mb);
+        return Err(anyhow::anyhow!("TOO_BIG"));
     }
 
     let mut reader = r.into_reader();
@@ -126,9 +127,12 @@ pub fn download(link: &str, file: &mut File) -> Result<(), String> {
     while let Ok(n) = reader.read(&mut buffer) {
         if n == 0 {
             if total_read == len {
-                println!("Download Finished. Downloaded {len_mb:.1}mb");
+                println!(
+                    "{}. Downloaded {len_mb:.1}mb ({total_read} bytes)",
+                    "Download Finished".green()
+                );
             } else {
-                println!("Breaking before finishing: {total_read}/{len}");
+                println!("{}: {total_read}/{len}", "Breaking before finishing".red());
             }
             break;
         }
@@ -141,9 +145,9 @@ pub fn download(link: &str, file: &mut File) -> Result<(), String> {
         if elapsed.as_millis() >= 1000 {
             let incremental = total_read - last_read;
             last_read = total_read;
-            let percentage = (total_read as f32/len as f32) * 100.0;
+            let percentage = (total_read as f32 / len as f32) * 100.0;
             let kbs = incremental / 1000;
-            
+
             println!("{percentage:>5.1}% | {kbs:>6} kb/s |");
             now = Instant::now();
         }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![allow(unused)]
-use std::{fs, path::PathBuf, str::FromStr};
+use std::{
+    fs::{self, remove_file},
+    path::PathBuf,
+    str::FromStr,
+};
 
 use select::{Album, Media};
 
@@ -7,16 +11,18 @@ mod get;
 mod io;
 mod select;
 
+use colored::Colorize;
 fn main() {
     let data = io::load_results().unwrap();
 
     for (idx, album) in data.iter().enumerate() {
-
         println!("Getting: {:?}", album.link);
 
         let mut path = PathBuf::from_str("/home/miguel/rust/downloader/test").unwrap();
 
         if let Some(title) = &album.title {
+            let title = title.replace("/", "");
+            println!("{}",title.yellow());
             path.push(title);
         } else {
             path.push(idx.to_string());
@@ -24,11 +30,15 @@ fn main() {
 
         if !path.exists() {
             fs::create_dir(&path);
+        } else {
+            println!("{} skipping..", path.to_str().unwrap().blue());
+            continue;
         }
 
         let body = get::get_album(album).unwrap();
         let media = select::get_album_media(body).unwrap();
 
+        let mut video_count = 0u16;
 
         for (idx, item) in media.into_iter().enumerate() {
             let mut media_path = path.clone();
@@ -38,12 +48,25 @@ fn main() {
                 Media::Photo(link) => {
                     media_path = media_path.with_extension("png");
                     let mut file = fs::File::create(&media_path).unwrap();
-                    get::download(link, &mut file);
+                    if let Err(err) = get::download(link, &mut file) {
+                        if err.to_string() == "TOO_BIG" {
+                            remove_file(media_path);
+                        }
+                    }
                 }
                 Media::Video(link) => {
+                    if video_count > 3 {
+                        println!("{}", "Video count greater than 3".red());
+                        break;
+                    }
                     media_path = media_path.with_extension("mp4");
                     let mut file = fs::File::create(&media_path).unwrap();
-                    get::download(link, &mut file);
+                    if let Err(err) = get::download(link, &mut file) {
+                        if err.to_string() == "TOO_BIG" {
+                            remove_file(media_path);
+                        }
+                    }
+                    video_count += 1;
                 }
             }
         }
