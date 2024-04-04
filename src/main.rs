1baf8626f4bf24710c1e7e5b63f1c627e88f62ed
miguel <vackap@gmail.com>
2024-04-04 18:54:19 -0400

    check existing

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,38 @@ use std::path::PathBuf;
 mod getter;
 mod select;
 
+fn check_downloaded() -> anyhow::Result<Vec<PathBuf>> {
+    let path = PathBuf::from("/home/miguel/blenders");
+
+    let mut result = Vec::with_capacity(10);
+
+    for dir in std::fs::read_dir(path)? {
+        let dir = match dir {
+            Ok(d) => d,
+            Err(err) => {
+                dbg!(err);
+                continue;
+            }
+        };
+
+        let metadata = dir.metadata().unwrap();
+
+        if metadata.is_dir() {
+            result.push(dir.path());
+        }
+    }
+
+    Ok(result)
+}
 fn main() {
+    let downloaded = check_downloaded().unwrap();
+
     let versions = ["4.1.1"];
 
     let links = getter::get_links().unwrap();
 
     for (key, link) in links.into_iter() {
         if versions.contains(&key.as_str()) {
-
             dbg!(&link);
 
             let filename = link
@@ -21,8 +45,13 @@ fn main() {
             let mut path = PathBuf::from("/home/miguel/blenders/");
             path.push(filename);
 
+            if downloaded.contains(&path.with_extension("").with_extension("")) {
+                println!("{} Already at Latest version", key);
+                continue;
+            }
+
             let mut file = std::fs::File::create(path).unwrap();
-            
+
             let download_result = getter::download(&link, &mut file);
 
             if download_result.is_err() {
