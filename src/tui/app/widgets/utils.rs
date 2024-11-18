38ec8b15e769035b4364a3d23bb28f9efc3ce1d3
miguel <vackap@gmail.com>
2024-11-17 22:51:43 -0300

    refactored utils

--- src/tui/app/widgets/utils.rs
+++ src/tui/app/widgets/utils.rs
@@ -4,25 +4,6 @@ use ratatui::text::Span;
 
 use crate::BlenderVersion;
 
-pub fn release_span(version: &BlenderVersion) -> Span<'_>  {
-    match version.release.as_str() {
-        x if x == "stable" => {
-            Span::styled(format!("{x:^11}"), Style::default().fg(Color::Green))
-        }
-        x if x == "beta" => {
-            Span::styled(format!("{x:^11}"), Style::default().fg(Color::Magenta))
-        }
-    
-        x if x == "candidate" => {
-            Span::styled(format!("{x:^11}"), Style::default().fg(Color::Red))
-        }
-        x if x == "alpha" => {
-            Span::styled(format!("{x:^11}"), Style::default().fg(Color::Gray))
-        }
-        _ => Span::styled(String::new(), Style::default().fg(Color::Red)),
-    }
-}
-
 pub fn version_span(version: &BlenderVersion) -> Span<'_> {
     match &version.version {
         x if x.contains("4.2") => {
@@ -32,5 +13,16 @@ pub fn version_span(version: &BlenderVersion) -> Span<'_> {
             Span::styled(format!("{x:^10}"), Style::default().bg(Color::Magenta))
         }
         x => Span::styled(format!("{x:^10}"), Style::default().bg(Color::Gray)),
+    }
+}
+
+pub fn release_span(version: &BlenderVersion) -> Span<'_> {
+    match version.release.as_str() {
+        x if x == "stable" => Span::styled(format!("{x:^11}"), Style::default().fg(Color::Green)),
+        x if x == "beta" => Span::styled(format!("{x:^11}"), Style::default().fg(Color::Magenta)),
+
+        x if x == "candidate" => Span::styled(format!("{x:^11}"), Style::default().fg(Color::Red)),
+        x if x == "alpha" => Span::styled(format!("{x:^11}"), Style::default().fg(Color::Gray)),
+        _ => Span::styled(String::new(), Style::default().fg(Color::Red)),
     }
 }
